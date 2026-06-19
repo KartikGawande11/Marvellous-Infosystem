118bcaa8faf1b869be5a5fb3e090e4b6fc784999
KartikGawande11 <kartikgawande19@gmail.com>
2026-06-19 12:56:35 +0530

    Create user_def_Function.py

---
+++ Python Module/user_def_Function.py
@@ -0,0 +1,9 @@
+def multiply(a, b):
+    return a * b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = multiply(num1, num2)
+
+print("Multiplication =", result)
